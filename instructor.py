d9f9ea73a91ab2d93239dfa3e3613b681cce8fc0
Luke Esworthy <lukesworthy@gmail.com>
2020-07-17 13:30:37 -0500

    initial commit after setting up file structure and defining classes

---
+++ instructor.py
@@ -0,0 +1,14 @@
+from student import Student
+
+
+class Instructor:
+
+    def __init__(self, first_name, last_name, slack_handle, specialty):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.slack_handle = slack_handle
+        self.cohort = ""
+        self.specialty = specialty
+
+    def assign_exercise(self):
+        Student.exercises.append(self)
